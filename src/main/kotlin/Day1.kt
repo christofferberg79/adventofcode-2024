60db8066cd5a3937578b58651ce8cf8e14b9cb7c
Christoffer Berg <christoffer.berg@collectum.se>
2024-12-06 01:17:47 +0100

    Day 1

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,9 +1,16 @@
 package cberg.aoc2024
 
-class Day1(private val input: List<String>) {
+import kotlin.math.abs
+
+class Day1(input: List<String>) {
     constructor(input: Input) : this(input.lines())
 
-    fun part1() = 0
+    private val lists = input.map { it.split("   ").map { it.toInt() } }.map { (a, b) -> a to b }.unzip()
 
-    fun part2() = 0
+    fun part1() = (lists.first.sorted() zip lists.second.sorted()).sumOf { abs(it.first - it.second) }
+
+    fun part2(): Int {
+        val frequency = lists.second.groupingBy { it }.eachCount()
+        return lists.first.sumOf { it * (frequency[it] ?: 0) }
+    }
 }
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -6,9 +6,9 @@ import kotlin.test.assertEquals
 class Test {
     @Test
     fun day1() {
-        assertEquals(0, Day1(Input("day1_ex1")).part1())
-        assertEquals(0, Day1(Input("day1")).part1())
-        assertEquals(0, Day1(Input("day1_ex1")).part2())
-        assertEquals(0, Day1(Input("day1")).part2())
+        assertEquals(11, Day1(Input("day1_ex1")).part1())
+        assertEquals(2970687, Day1(Input("day1")).part1())
+        assertEquals(31, Day1(Input("day1_ex1")).part2())
+        assertEquals(23963899, Day1(Input("day1")).part2())
     }
 }
